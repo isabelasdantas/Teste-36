300660363667ab8f286974beb7c6295a91002031
Isa Dantas <157421767+isabelasdantas@users.noreply.github.com>
2024-02-02 18:57:45 -0300

    Add files via upload

---
+++ TESTE36.py
@@ -0,0 +1,16 @@
+casa = float(input('Qual o valor da casa? R$: '))
+salario = float(input('Quanto você recebe? R$: '))
+anos = int(input('Em quanto anos você quer pagar?'))
+mensal = casa / (anos * 12)
+porcent = mensal * 30 / 100
+print('Para pagar uma casa no valor de R${:.2f} em {} anos, a prestação fica no valor de R${:.2f}.'
+.format(casa, anos, mensal))
+if (salario <= porcent):
+        print('Empréstimo pode ser CONCEDIDO')
+else:
+        print('Empréstimo foi NEGADO')
+
+
+
+
+
